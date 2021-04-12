6a037ce315d3e2253a0019490205be7b351a59de
Dao Hai Linh <linhdh@live.com>
2021-04-12 19:31:58 +0700

    add atmel studio project

---
+++ u8g2oled091/u8g2oled091/ArduinoCore/library.cpp
@@ -0,0 +1,16 @@
+/*
+ * ArduinoCore.cpp
+ *
+ * Created: 4/12/2021 7:12:36 PM
+ * Author : linhd
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
